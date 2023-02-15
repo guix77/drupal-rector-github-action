c2d85af85682af369edc74681d4817e9a8cbc8bc
Guillaume Duveau <guillaume.duveau@gmail.com>
2023-02-15 14:08:10 +0100

    problem matcher

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+cp /problemMatcher.json /github/workflow/problemMatcher.json
+echo "::add-matcher::${RUNNER_TEMP}/_github_workflow/problemMatcher.json"
 composer install
 vendor/bin/rector process web/modules/custom --dry-run
 status=$?
+echo "::remove-matcher owner=rector::"
 exit $status
