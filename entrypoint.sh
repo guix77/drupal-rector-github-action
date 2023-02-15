57c60d0085ca9a95190581b6317e854656171ce8
Guillaume Duveau <guillaume.duveau@gmail.com>
2023-02-15 15:46:06 +0100

    up

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-cp /problemMatcher.json /github/workflow/problemMatcher.json
-echo "::add-matcher::${RUNNER_TEMP}/_github_workflow/problemMatcher.json"
+cp /problemMatcher.json .
+echo "::add-matcher::problemMatcher.json"
 composer install
 vendor/bin/rector process web/modules/custom --dry-run
 status=$?
